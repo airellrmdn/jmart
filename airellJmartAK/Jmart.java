1ed8fe1f7091846d53fbb4d203af52863fa76c97
Airell Ramadhan Budiraharjo <airell.ramadhan@ui.ac.id>
2021-09-16 18:07:00 +0700

    Modul 1 - Revisi

--- airellJmartAK/Jmart.java
+++ airellJmartAK/Jmart.java
@@ -2,24 +2,47 @@ package airellJmartAK;
 
 public class Jmart
 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        
     }
     
-    public static int getPromo (){
+    public static int getPromo(){
         return 0;
     }
     
-    public static String getCustomer (){
+    public static String getCustomer(){
         return "OOP";
     }
     
-    public static float getDiscountPercentage (int sebelum, int sesudah){
-        if (sebelum < sesudah){
+    public static float getDiscountPercentage(int before, int after){
+        if (before < after || before == after){
             return 0.0f;
         }
-        else {
-            return ((sebelum - sesudah) / sebelum) * 100;
+        return 100.0f * (before - after) / before;
+    }
+    
+    public static float getDiscountedPrice(int price, float discountPercentage){
+        if (discountPercentage >= 100.0f){
+            return 0;
         }
+        float cut = price * discountPercentage / 100.0f;
+        return price - (int) cut;
+    }
+    
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage){
+        float divider = 100.0f - discountPercentage;
+        return (int) (discountedPrice * 100 / divider);
+    }
+    
+    public static float getCommissionMultiplier(){
+        return 0.05f;
+    }
+    
+    public static int getAdjustedPrice(int price){
+        return (int) (price + (price * getCommissionMultiplier()));
+    }
+    
+    public static int getAdminFee(int price){
+        return (int) (price * getCommissionMultiplier());
     }
 }
