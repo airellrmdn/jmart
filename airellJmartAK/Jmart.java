2fc4cb54cf6d8e042f73136ef68d27c43884d223
Airell Ramadhan Budiraharjo <airell.ramadhan@ui.ac.id>
2021-09-20 19:00:17 +0700

    CS Modul 2 - Revisi

--- airellJmartAK/Jmart.java
+++ airellJmartAK/Jmart.java
@@ -5,4 +5,8 @@ public class Jmart
     public static void main(String[] args){
         
     }
+    
+    public static Product create(){
+        return new Product("Bike", 1, true, new PriceTag(100), ProductCategory.TOYS);
+    }
 }
--- airellJmartAK/Product.java
+++ airellJmartAK/Product.java
@@ -2,9 +2,23 @@ package airellJmartAK;
 
 public class Product
 {
-
-    public Product(String name){
-        
+    private static int idCounter = 0;
+    public int id;
+    public String name;
+    public int weight;
+    public boolean conditionUsed;
+    public PriceTag pricetag;
+    public ProductCategory category;
+    public ProductRating rating;
+    
+    public Product(String name, int weight, boolean conditionUsed, PriceTag pricetag, ProductCategory category){
+        this.id = idCounter++;
+        this.name = name;
+        this.weight = weight;
+        this.conditionUsed = conditionUsed;
+        this.pricetag = pricetag;
+        this.category = category;
+        this.rating = new ProductRating();
     }
 
     
