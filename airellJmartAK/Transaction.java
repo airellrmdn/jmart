a95f0825a49c0a3aee58f33bc3bf2e9ededf9e50
Airell Ramadhan Budiraharjo <airell.ramadhan@ui.ac.id>
2021-09-25 10:57:39 +0700

    Modul 3 - CS

--- airellJmartAK/Transaction.java
+++ airellJmartAK/Transaction.java
@@ -5,6 +5,11 @@ public class Transaction extends Recognizable
     public String time;
     public int buyerId;
     public int storeId;
+    public Rating rating;
+    
+    public enum Rating{
+        NONE, BAD, NEUTRAL, GOOD
+    }
     
     protected Transaction(int id, int buyerId, int storeId){
         super(id);
